764b1c8c41c1db88f3e9a8a2b3f4e930e67a3568
arghyaiitb <arghyasaha26@gmail.com>
2017-05-24 00:38:39 +0530

    localdb connection sucessfull

--- app.js
+++ app.js
@@ -2,46 +2,34 @@ var express = require('express');
 
 // Mongoose import
 var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
 
-// Mongoose connection to MongoDB (ted/ted is readonly)
-mongoose.connect('mongodb://ted:ted@ds061797.mongolab.com:61797/theenlighteneddeveloper', function (error) {
-    if (error) {
-        console.log(error);
+var Cat = mongoose.model('Cat', { name: String });
+
+var kitty = new Cat({ name: 'Zildjian' });
+kitty.save(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('meow');
     }
 });
 
-// Mongoose Schema definition
-var Schema = mongoose.Schema;
-var UserSchema = new Schema({
-    first_name: String,
-    last_name: String,
-    email: String
-});
 
 // Mongoose Model definition
-var User = mongoose.model('users', UserSchema);
 
 // Bootstrap express
 var app = express();
 
+app.get('/', function (req, res) {
+    res.send("<a href='/data'>Show data</a>");
+});
 // URLS management
 
-app.get('/', function (req, res) {
-    res.send("<a href='/users'>Show Users</a>");
-});
-
-app.get('/users', function (req, res) {
-    User.find({}, function (err, docs) {
+app.get('/data', function (req, res) {
+    Cat.find({}, function (err, docs) {
         res.json(docs);
     });
-});
-
-app.get('/users/:email', function (req, res) {
-    if (req.params.email) {
-        User.find({ email: req.params.email }, function (err, docs) {
-            res.json(docs);
-        });
-    }
 });
 
 // Start the server
